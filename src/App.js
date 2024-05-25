afc7a8188f6a6efe438ba536b97a1ef5e92c21ec
kunwarrohit <rohitsingh95650@gmail.com>
2024-05-25 22:59:40 +0530

    second

--- src/App.js
+++ src/App.js
@@ -4,24 +4,13 @@ import FormDataContext from "./components/FormDataContext/FormDataContext";
 import HomeScreen from "./screens/HomeScreen";
 
 function App() {
-  const [formDetails, setFormDetails] = useState({
-    name: "",
-    collegeduniaRating: "",
-    placement: "",
-    Ranking: "",
-    fees: "",
-    userReviewRating: "",
-    featured: "",
-  });
   return (
     <>
       <BrowserRouter>
         <h1 className="text-center underline text-2xl">List of Collages</h1>
-        <FormDataContext.Provider value={[formDetails, setFormDetails]}>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
-        </FormDataContext.Provider>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
